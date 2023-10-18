6e677284ea9830b5ebdba4b49444817738f13ca8
“javascriptseo” <seofreelance2021@outlook.com>
2023-10-18 14:26:38 -0700

    basic application setup

---
+++ main.py
@@ -0,0 +1,13 @@
+# here I am getting the user inputs for test score and class rank
+studentName = input("Type your name here")
+questionOne = input("What is 1 + 2?")
+
+# then I am converting the string to integer with the int function
+answerOne = int(questionOne)
+
+# The rest of the code checks the admissions acceptance
+# if the score is greater than or equal to 90, and the classrank is greater or equal to 25
+if answerOne == 3:
+    print("True")
+else:
+    print("False")
