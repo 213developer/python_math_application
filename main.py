30cf327bcfa36222bafb3306421fa068a788d200
“javascriptseo” <seofreelance2021@outlook.com>
2023-10-18 14:35:11 -0700

    added functionality to accumulate totals, and to print the students score

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ answerOne = int(questionOne)
 # The rest of the code checks the admissions acceptance
 # if the score is greater than or equal to 90, and the classrank is greater or equal to 25
 if answerOne == 3:
+    score1 = 0
     print("True")
 else:
     print("False")
+    score1 = 1
+
+scoreOne = str(score1)
+
+print("Chris's Score is " + scoreOne + " Out of 10")
